771c39ed92fd28372b30d0ccfe8e06990c9e043c
messi <zengwei@newborn-town.com>
2016-11-18 15:04:32 +0800

    add:增加白名单模块

---
+++ app/src/main/java/com/solo/security/contansts/SecurityConstants.java
@@ -0,0 +1,12 @@
+package com.solo.security.contansts;
+
+/**
+ * Created by Messi on 16-11-18.
+ */
+
+public class SecurityConstants {
+
+    public static final String PREFS_KEY_MEMORY_WHITE_LIST = "memory_white_list";
+
+    public static final String PREFS_KEY_SAFE_WHITE_LIST = "safe_white_list";
+}
--- app/src/main/java/com/solo/security/data/whitelistsource/WhiteListDataImpl.java
+++ app/src/main/java/com/solo/security/data/whitelistsource/WhiteListDataImpl.java
@@ -3,6 +3,14 @@ package com.solo.security.data.whitelistsource;
 import android.content.Context;
 
 import com.google.common.base.Preconditions;
+import com.solo.security.contansts.SecurityConstants;
+import com.solo.security.data.Security;
+import com.solo.security.prefs.SecurityPreference;
+import com.solo.security.utils.AppUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Messi on 16-11-4.
@@ -25,7 +33,40 @@ public class WhiteListDataImpl implements WhiteListData {
     }
 
     @Override
-    public void loadWhiteList(WhiteListDataCallback callback) {
+    public void loadMemoryWhiteList(WhiteListDataCallback callback) {
+        Set<String> memoryPkgs = SecurityPreference.getStringSet(mContext, SecurityConstants.PREFS_KEY_MEMORY_WHITE_LIST);
+        if (memoryPkgs == null || memoryPkgs.isEmpty()) {
+            callback.onEmptyMemoryList();
+        } else {
+            List<Security> securities = new ArrayList<>();
+            for (String pkg : memoryPkgs) {
+                Security security = new Security();
+                security.setPackageName(pkg);
+                security.setIcon(AppUtils.getApplicationIcon(mContext, pkg));
+                security.setLabel((String) AppUtils.getApplicationLabel(mContext, pkg));
 
+                securities.add(security);
+            }
+            callback.onMemoryListLoaded(securities);
+        }
+    }
+
+    @Override
+    public void loadSafeWhiteList(WhiteListDataCallback callback) {
+        Set<String> safePkgs = SecurityPreference.getStringSet(mContext, SecurityConstants.PREFS_KEY_SAFE_WHITE_LIST);
+        if (safePkgs == null || safePkgs.isEmpty()) {
+            callback.onEmptySafeList();
+        } else {
+            List<Security> securities = new ArrayList<>();
+            for (String pkg : safePkgs) {
+                Security security = new Security();
+                security.setPackageName(pkg);
+                security.setIcon(AppUtils.getApplicationIcon(mContext, pkg));
+                security.setLabel((String) AppUtils.getApplicationLabel(mContext, pkg));
+
+                securities.add(security);
+            }
+            callback.onSafeListLoaded(securities);
+        }
     }
 }
